5606cde12ffeead1ea2dd783cd74c755f37d55a5
Olasunkanmi <alabiolasunkanmi@outlook.com>
2019-01-08 23:49:44 +0100

    make responsive
    make hero carousel responsice. Fix 'interested' section carousel and make responsive. make 'best rated' responsive

--- js/main.js
+++ js/main.js
@@ -43,6 +43,43 @@ $(document).ready(function () {
             }
         ]
     });
+
+    $('.interest__container').slick({
+        slidesToShow: 3,
+        slidesToScroll: 1,
+        autoplay: true,
+        autoplaySpeed: 2000,
+        variableWidth: true,
+        infinite: true,
+        arrows: false,
+        dots: true,
+        responsive: [{
+                breakpoint: 1024,
+                settings: {
+                    slidesToShow: 2,
+                    slidesToScroll: 1,
+                    infinite: true,
+                }
+            },
+            {
+                breakpoint: 600,
+                settings: {
+                    slidesToShow: 2,
+                    slidesToScroll: 1
+                }
+            },
+            {
+                breakpoint: 480,
+                settings: {
+                    slidesToShow: 1,
+                    slidesToScroll: 1
+                }
+            }
+            // You can unslick at a given breakpoint now by adding:
+            // settings: "unslick"
+            // instead of a settings object
+        ]
+    });
 });
 
 $(document).ready(function () {
