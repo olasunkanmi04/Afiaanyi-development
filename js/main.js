2830f4fe5ad71d24222d057e606958311b6da93c
Olasunkanmi <alabiolasunkanmi@outlook.com>
2019-01-21 20:59:27 +0100

    marketplace build

--- js/main.js
+++ js/main.js
@@ -190,6 +190,42 @@ $(document).ready(function () {
         ]
     });
 
+    $('.marketplace__stores-product-slider').slick({
+        dots: false,
+        infinite: false,
+        speed: 300,
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        variableWidth: true,
+        // centerMode: true,
+        responsive: [{
+                breakpoint: 1024,
+                settings: {
+                    slidesToShow: 1,
+                    slidesToScroll: 1,
+                    infinite: true,
+                    dots: false
+                }
+            },
+            {
+                breakpoint: 900,
+                settings: {
+                    slidesToShow: 1,
+                    slidesToScroll: 1
+                }
+            },
+            {
+                breakpoint: 480,
+                settings: {
+                    slidesToShow: 1,
+                    slidesToScroll: 1
+                }
+            }
+            // You can unslick at a given breakpoint now by adding:
+            // settings: "unslick"
+            // instead of a settings object
+        ]
+    });
 
 
 
