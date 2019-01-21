6897eb2a9a3eeff37fd81d218b67e827c5dfe857
Olasunkanmi <alabiolasunkanmi@outlook.com>
2019-01-21 09:19:37 +0100

    add recommended section

--- js/main.js
+++ js/main.js
@@ -164,17 +164,17 @@ $(document).ready(function () {
         responsive: [{
                 breakpoint: 1024,
                 settings: {
-                    slidesToShow: 3,
-                    slidesToScroll: 3,
+                    slidesToShow: 2,
+                    slidesToScroll: 2,
                     infinite: true,
-                    dots: true
+                    dots: false
                 }
             },
             {
-                breakpoint: 600,
+                breakpoint: 900,
                 settings: {
-                    slidesToShow: 2,
-                    slidesToScroll: 2
+                    slidesToShow: 1,
+                    slidesToScroll: 1
                 }
             },
             {
